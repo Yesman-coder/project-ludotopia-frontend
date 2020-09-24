6ecef3e1404f65e955c70d6fe1232ac995589b49
Yesman-coder <yesmanshiva@gmail.com>
2020-09-24 18:41:54 +0000

    funcionalidad de login

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,20 +1,23 @@
 const getState = ({ getStore, getActions, setStore }) => {
+    const baseUrl= "https://3000-e138cd05-a862-42ad-8343-3b3ea10be3af.ws-us02.gitpod.io/"
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			users: []
 		},
 		actions: {
+            login: async (username, password) => {
+				let actions = getActions();
+				if (username && password != "") {
+					const response = await fetch(`${baseUrl}/todos`, {
+						method: "POST",
+						body: JSON.stringify({ label: `${task}`, done: false }),
+						headers: {
+							"Content-Type": "application/json"
+						}
+					});
+					await actions.getList();
+				} else alert("Please type a new task");
+			},
 			// Use getActions to call a function within a fuction
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
