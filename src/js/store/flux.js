3f50aa41217f0278f1b0e9ce8e190d1ee291f310
borisbrunop <borisbruno88@gmail.com>
2020-10-09 18:49:23 -0400

    quite notas de createBet y puse nota de link de backend de boris en flux

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,5 +1,6 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	const baseUrl = "http://0.0.0.0:4000";
+	const baseUrl = "http://0.0.0.0:3000";
+	//const baseUrl = "http://0.0.0.0:3000";
 	//const baseUrl = "https://3000-a36fb042-3935-4d02-805d-4fb354c87a20.ws-us02.gitpod.io/";
 
 	//Recordar verificar URL
